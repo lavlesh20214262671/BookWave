736bf869c9d31272b197d9b41f72785419d251b2
lavlesh <baislavleshkumar@gmail.com>
2024-08-16 03:34:42 +0530

    First commit

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            Welcom LK book Store
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+          Welcome to our bookstore! We're delighted to have you here. Explore a world of captivating stories, insightful non-fiction, and timeless classics. Whether you're searching for your next great read or just browsing, we're here to help. Enjoy your visit, and thank you for being a part of our reading community!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
